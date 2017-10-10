aa94b0e344d0561821669019ea97163b0d2486c4
cfeimst1 <cfeimster@live.com>
2017-10-10 02:56:50 +0000

    Done with all sections. Forgot to do this step.

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,59 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id]
+    @movie = Movie.find(id) 
+  end
+  
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    # params.require(:movie)
+    # permitted = params[:movie].permit(:title,:rating,:release_date)
+    # @movie = Movie.create!(permitted)
+    # flash[:notice] = "#{@movie.title} was successfully created."
+    # redirect_to movies_path
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
+  
+  def update
+    # @movie = Movie.find params[:id]
+    # permitted = params[:movie].permit(:title,:rating,:release_date)
+    # @movie.update_attributes!(permitted)
+    # flash[:notice] = "#{@movie.title} was successfully updated."
+    # redirect_to movie_path(@movie)
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
+  # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+  
+end
